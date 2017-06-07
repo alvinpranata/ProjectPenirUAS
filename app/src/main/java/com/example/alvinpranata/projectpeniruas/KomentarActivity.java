0ec5a5f7c3997eacbfd6d2c5bbc9a04a59fc846a
alvinpranata <alvinpranata3@gmail.com>
2017-06-07 22:02:53 +0700

    a

---
+++ app/src/main/java/com/example/alvinpranata/projectpeniruas/KomentarActivity.java
@@ -0,0 +1,13 @@
+package com.example.alvinpranata.projectpeniruas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KomentarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_komentar);
+    }
+}
